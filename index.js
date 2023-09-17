aa882ef120aeffc0f43611132f5af0f98b5afebc
Dante de Araujo <danteeng@hotmail.com>
2023-09-17 19:52:46 -0300

    Terminando a pokeapi

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const loadMoreButton = document.getElementById("loadButton")
 const limit = 9;
 let offset = 0;
 const maxRecord = 151;
+let scrollTop = 0;
+let scrollLeft = 0;
 
 function convertPokemonToHTML(pokemon){
   let typesString = pokemon.types.map((type)=>{
     return `<li class="type ${type.type.name}">${type.type.name}</li>`
   })
   let endString = `
-      <li class="pokemon ${pokemon.types[0].type.name}">
+      <li class="pokemon ${pokemon.types[0].type.name}")>
         <span class="number">#${pokemon.id.toString().padStart(3,'0')}</span>
         <span class="name">${pokemon.name}</span>
         <div class="details">
@@ -26,9 +28,65 @@ function convertPokemonToHTML(pokemon){
 async function loadPokemonItems(offset=0,limit=5){
     const pokemonList = document.getElementById("pokemonList");
     let pokemons = await pokeapi.getPokemons(offset,limit);
-    pokemons.forEach(pokemon => {
+    pokemons.forEach((pokemon,idx) => {
       pokemonList.innerHTML += convertPokemonToHTML(pokemon);
     });
+    let cards = document.getElementsByClassName("pokemon");
+    for (let i = 0; i < cards.length; i++){
+      let name = cards[i].getElementsByClassName("name")[0].textContent;
+      cards[i].addEventListener('click',()=>openPokemonModal(name))
+    }
+
+}
+
+async function openPokemonModal(name){
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+  window.scrollTo({top:0, behavior:'smooth'});
+  let pokemon = await pokeapi.getPokemon(name)
+  let modal = document.getElementById("modal");
+  let typesString = pokemon.types.map((type)=>{
+    return `<li class="details type ${type.type.name}">${type.type.name}</li>`
+  })
+  let statsString = pokemon.stats.map((stat)=>{
+    return `<li class="details stat ${stat.stat.name}" style="text-align:left">${stat.stat.name}</li>`
+  })
+  console.log(pokemon.types[0].type.name)
+  let statsData = pokemon.stats.map((stat)=>{
+    return `<div class="${pokemon.types[0].type.name}" style="filter:brightness(0.9);width:300px"><li class="details stat bar ${pokemon.types[0].type.name}" style="filter:brightness(2.0);width: ${stat.base_stat}%;text-align:center; max-width:100%">${stat.base_stat}</li></div>`
+  })
+  modal.innerHTML = `
+      <div class="detailCard ">
+        <div class="details header">
+          <img src=${pokemon.sprites.other["home"].front_default} alt="${pokemon.name}">
+          </img>
+        </div>
+        <div class="details content ${pokemon.types[0].type.name}">
+          <div class="details title">
+            <h2 class="details name">${pokemon.name}</h2>
+            <span class="details number">#${pokemon.id.toString().padStart(3,'0')}</span>
+          </div>
+          <div class="details types container">
+            <ol class="details types">
+              ${typesString.join('')}
+            </ol>
+          </div>
+          <div class="details info container">
+            <div style="display:flex; flex-direction: row; justify-content:center; width:100%;">
+            <ol>
+              ${statsString.join('')}
+            </ol>
+            <ol>
+              ${statsData.join('')}
+            </ol>
+            </div>
+          </div>
+        <btn class="modal-btn-close" onclick="closeModal()">Fechar</btn>
+        </div>
+      </div>
+  `
+  modal.style.display = 'flex'
+
 }
 
 loadMoreButton.addEventListener('click', () => {
@@ -47,3 +105,9 @@ loadMoreButton.addEventListener('click', () => {
 
 loadPokemonItems(offset,limit);
 
+
+function closeModal(){
+  let modal = document.getElementById("modal");
+  modal.style.display = 'none'
+  window.scrollTo({top:scrollTop, behavior:'smooth'});
+}
--- poke-api.js
+++ poke-api.js
@@ -10,14 +10,44 @@ pokeapi.getPokemons = async (offset=0,limit=10) => {
   let data = await Promise.all(results.map(async (pokemon) => {
     let res = await fetch(pokemon.url)
     let data = await res.json();
-    // console.log(data);
     return data;
   }))
 
   return data;
 }
 
-// pokeapi.getPokemonDetail = async (pokemon){
-//   // const pokemons = getPokemons()
-// }
+class Pokemon {
+  number;
+  name;
+  type;
+  types = [];
+  photo;
+}
+
+function convertPokemonDetailToPokemon(pokemonDetail){
+  const pokemon = Pokemon();
+  pokemon.number = pokemonDetail.id;
+  pokemon.name = pokemonDetail.name;
+
+  const types = pokemonDetail.map((typesSlot)=>typesSlot.name)
+  const [type] = types
+  pokemon.types = types;
+  pokemon.type = type
+  pokemon.photo = pokemonDetail.sprites.other.dream_world.front_default;
+
+  return pokemon;
+
+}
+
+pokeapi.getPokemonDetails = async (pokemon) => {
+  let detail = await fetch(pokemon.url);
+  let json = await detail.json()
+  return convertPokemonDetailToPokemon(pokemonDetail);
+}
+
+pokeapi.getPokemon = async (name) =>{
+  let data = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`)
+  let json = await data.json();
+  return json;
+}
 
